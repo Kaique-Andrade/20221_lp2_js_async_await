1d6ee4a8abd71eff57ba7f47aaa5aa364cfaf397
Kaique <kaique_andrade17@hotmail.com>
2022-03-12 12:28:17 -0300

    url

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-https://api.openweathermap.org/data/2.5/forecast?q=saocaetanodosul&appid={API key}
+require('dotenv').config()
 
+//const PROTOCOL = process.env.PROTOCOL
+//const BASE_URL = process.env.BASE_URL
+//const APPID = process.env.APPID
+//const LANGUAGE = process.env.LANGUAGE
+//const UNITS = process.env.UNITS
 
+const { PROTOCOL, BASE_URL, APPID, LANGUAGE, UNITS, Q} = process.env
 
-https://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@github.com/Kaique-Andrade/20221_lp2_js_async_await.git
+//const url = PROTOCOL + '://'
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&q=${Q}`
