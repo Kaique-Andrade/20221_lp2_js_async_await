ebc273f2b1fd8587a315ee917a92ed264019e996
Kaique <kaique_andrade17@hotmail.com>
2022-03-12 11:45:20 -0300

    Instalamos o dotenv e a axios

---
+++ index.js
@@ -0,0 +1,5 @@
+https://api.openweathermap.org/data/2.5/forecast?q=saocaetanodosul&appid={API key}
+
+
+
+https://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@github.com/Kaique-Andrade/20221_lp2_js_async_await.git
